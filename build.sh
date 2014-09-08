96b41dc8aca915a8a459b30710b1570ec351bb12
Tuomas Starck <dev@starck.fi>
2014-09-08 11:51:11 +0000

    Automatic build for two dists

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. /etc/lsb-release || true
+
+if [ "$DISTRIB_CODENAME" == "precise" ]; then
+cat >debian/packages-available.install <<EOL
+packagesavailable.rb  /usr/lib/ruby/vendor_ruby/facter/
+EOL
+fi
+
+if [ "$DISTRIB_CODENAME" == "trusty" ]; then
+cat >debian/packages-available.install <<EOL
+packagesavailable.rb  /usr/lib/ruby/1.8/facter/
+EOL
+fi
+
+dpkg-buildpackage -rfakeroot -b
