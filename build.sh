0b04ba36aed7f66c8d10e4a1a5cc2b437b574f7d
Tuomas Starck <dev@starck.fi>
2014-09-09 15:04:08 +0000

    Fix dist mixup

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 . /etc/lsb-release || true
 
-if [ "$DISTRIB_CODENAME" == "precise" ]; then
+if [ "$DISTRIB_CODENAME" == "trusty" ]; then
 cat >debian/packages-available.install <<EOL
 packagesavailable.rb  /usr/lib/ruby/vendor_ruby/facter/
 EOL
 fi
 
-if [ "$DISTRIB_CODENAME" == "trusty" ]; then
+if [ "$DISTRIB_CODENAME" == "precise" ]; then
 cat >debian/packages-available.install <<EOL
 packagesavailable.rb  /usr/lib/ruby/1.8/facter/
 EOL
